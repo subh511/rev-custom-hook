d49228dd1259f83c20aa82432b9a78f16e36cccb
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-04-18 22:21:30 +0530

    userIsOnline

--- rev-custom-hooks/src/App.jsx
+++ rev-custom-hooks/src/App.jsx
@@ -1,11 +1,13 @@
 import DataFetching from "./components/DataFetching";
 import Rendering from "./components/Rendering";
+import UserOnline from "./components/UserOnline";
 function App() {
 
   return(
     <div>
     {/*<Rendering/>*/}
     {/*<DataFetching/>*/}
+    <UserOnline/>
     </div>
   )
 }
---
+++ rev-custom-hooks/src/components/UserOnline.jsx
@@ -0,0 +1,30 @@
+//import React from 'react';
+import { useEffect, useState } from "react";
+
+function useIsOnline() {
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+
+  useEffect(()=>{
+    window.addEventListener("online", ()=>{setIsOnline(true)});
+
+    window.addEventListener("offline", ()=>{setIsOnline(false)});
+  },[])
+
+  return isOnline;
+}
+export default function UserOnline() {
+  const isOnline = useIsOnline();
+
+  // if(isOnline){
+  //     return "yay ur online!"
+  // }
+
+  // return "your are offline, sorry for your Inconvience"
+  return (
+    <div>
+      {isOnline
+        ? "yay ur online!"
+        : "your are offline, sorry for your Inconvience"}
+    </div>
+  );
+}
